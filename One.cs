4224ed168bb274f8b849c7a7603bca4a861d5de0
Benjamin E. Iserman <winggar1228@gmail.com>
2021-12-02 22:51:58 -0600

    Add project files.

---
+++ One.cs
@@ -0,0 +1,60 @@
+﻿// Part 1
+/*
+List<int> depths = new();
+
+while (true)
+{
+    string s = Console.ReadLine();
+    if (string.IsNullOrEmpty(s)) break;
+    depths.Add(int.Parse(s));
+}
+
+int count = 0;
+for (int i = 0; i < depths.Count - 1; i++)
+{
+    if (depths[i + 1] > depths[i]) count++;
+}
+
+Console.WriteLine($"Solution is: {count}");
+*/
+// Part 2 (there's a better solution here by using a sliding window but I didn't do that)
+/*
+List<int> depths = new();
+
+while (true)
+{
+    string s = Console.ReadLine();
+    if (string.IsNullOrEmpty(s)) break;
+    depths.Add(int.Parse(s));
+}
+
+int count = 0;
+int prevSum = int.MaxValue, sum;
+for (int i = 0; i < depths.Count - 2; i++)
+{
+    sum = depths[i] + depths[i + 1] + depths[i + 2];
+    if (sum > prevSum) count++;
+    prevSum = sum;
+}
+
+Console.WriteLine($"Solution is: {count}");
+*/
+// Part 2b (here's the better solution)
+/*
+List<int> depths = new();
+
+while (true)
+{
+    string s = Console.ReadLine();
+    if (string.IsNullOrEmpty(s)) break;
+    depths.Add(int.Parse(s));
+}
+
+int count = 0;
+for (int i = 0; i < depths.Count - 3; i++)
+{
+    if (depths[i + 3] > depths[i]) count++;
+}
+
+Console.WriteLine($"Solution is: {count}");
+*/
---
+++ Two.cs
@@ -0,0 +1,55 @@
+﻿// Part 1
+/*
+int location = 0, depth = 0;
+while (true)
+{
+    string got = Console.ReadLine();
+    if (string.IsNullOrWhiteSpace(got)) break;
+
+    string[] instruction = got.Split();
+    int x = int.Parse(instruction[1]);
+
+    switch (instruction[0])
+    {
+        case "forward":
+            location += x;
+            break;
+        case "up":
+            depth -= x;
+            break;
+        case "down":
+            depth += x;
+            break;
+    }
+}
+
+Console.WriteLine($"Key: {location * depth}");
+*/
+// Part 2
+/*
+long location = 0, depth = 0, aim = 0;
+while (true)
+{
+    string got = Console.ReadLine();
+    if (string.IsNullOrWhiteSpace(got)) break;
+
+    string[] instruction = got.Split();
+    int x = int.Parse(instruction[1]);
+
+    switch (instruction[0])
+    {
+        case "forward":
+            location += x;
+            depth += aim * x;
+            break;
+        case "up":
+            aim -= x;
+            break;
+        case "down":
+            aim += x;
+            break;
+    }
+}
+
+Console.WriteLine($"Key: {location * depth}");
+*/
