16635462fcb7b1592f5b2e226ac6e30c73ab865d
Benjamin E. Iserman <winggar1228@gmail.com>
2021-12-05 23:36:49 -0600

    day 6

--- 6-1.cs
+++ 6-1.cs
@@ -1,3 +1,19 @@
 ﻿/*
+List<int> fish = (from x in Console.ReadLine().Split(',') select int.Parse(x)).ToList();
 
+for (int i = 0; i < 80; i++)
+{
+    int count = fish.Count;
+    for (int j = 0; j < count; j++)
+    {
+        fish[j]--;
+        if (fish[j] == -1)
+        {
+            fish[j] = 6;
+            fish.Add(8);
+        }
+    }
+}
+
+Console.WriteLine(fish.Count);
 */
--- 6-2.cs
+++ 6-2.cs
@@ -1,3 +1,33 @@
 ﻿/*
+using System.Numerics;
 
+List<int> fish = (from x in Console.ReadLine().Split(',') select int.Parse(x)).ToList();
+Dictionary<int, BigInteger> dict = new();
+
+for (int i = 0; i <= 8; i++) dict.Add(i, 0);
+
+foreach (int f in fish) dict[f]++;
+
+for (int i = 0; i < 256; i++)
+{
+    BigInteger carry = dict[0];
+
+    for (int j = 0; j < 8; j++)
+    {
+        dict[j] = dict[j + 1];
+    }
+
+    dict[8] = 0;
+
+    dict[6] += carry;
+    dict[8] += carry;
+}
+
+BigInteger sum = 0;
+foreach (BigInteger value in dict.Values)
+{
+    sum += value;
+}
+
+Console.WriteLine(sum);
 */
