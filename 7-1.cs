384439ddc8817f1ed68fa3c2d0e004daa214eb3e
Benjamin E. Iserman <winggar1228@gmail.com>
2021-12-06 23:12:05 -0600

    day 7

--- 7-1.cs
+++ 7-1.cs
@@ -1,3 +1,18 @@
 ﻿/*
+int[] positions = (from string s in Console.ReadLine().Split(',') select int.Parse(s)).ToArray();
 
+int minFuel = int.MaxValue;
+
+for (int i = 0; i < positions.Length; i++)
+{
+    int fuel = 0;
+    for (int j = 0; j < positions.Length; j++)
+    {
+        fuel += Math.Abs(i - positions[j]);
+    }
+
+    if (fuel < minFuel) minFuel = fuel;
+}
+
+Console.WriteLine(minFuel);
 */
--- 7-2.cs
+++ 7-2.cs
@@ -1,3 +1,27 @@
 ﻿/*
+int[] positions = (from string s in Console.ReadLine().Split(',') select int.Parse(s)).ToArray();
 
+long minFuel = long.MaxValue;
+
+int max = positions.Max();
+for (int i = 0; i < max; i++)
+{
+    long fuel = 0;
+    for (int j = 0; j < positions.Length; j++)
+    {
+        int x = Math.Abs(i - positions[j]);
+
+        int f = 0;
+        for (int k = 1; k <= x; k++)
+        {
+            f += k;
+        }
+
+        fuel += f;
+    }
+
+    if (fuel < minFuel) minFuel = fuel;
+}
+
+Console.WriteLine(minFuel);
 */
