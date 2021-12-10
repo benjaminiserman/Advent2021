dc824a07f7cd58da9f16c7f1fb07ee29576fb18d
Benjamin E. Iserman <winggar1228@gmail.com>
2021-12-09 23:42:39 -0600

    day 10

--- a10-1.cs
+++ a10-1.cs
@@ -1,3 +1,40 @@
 ﻿/*
+Dictionary<char, char> braces = new()
+{
+    { '(', ')' },
+    { '[', ']' },
+    { '{', '}' },
+    { '<', '>' },
+};
 
+Dictionary<char, int> counts = new()
+{
+    { ')', 0 },
+    { ']', 0 },
+    { '}', 0 },
+    { '>', 0 },
+};
+
+Stack<char> stack = new();
+
+while (true)
+{
+    string s = Console.ReadLine();
+    if (string.IsNullOrWhiteSpace(s)) break;
+
+    for (int i = 0; i < s.Length; i++)
+    {
+        if (braces.ContainsKey(s[i])) stack.Push(s[i]);
+        else
+        {
+            if (s[i] != braces[stack.Pop()])
+            {
+                counts[s[i]]++;
+                break;
+            }
+        }
+    }
+}
+
+Console.WriteLine(counts[')'] * 3 + counts[']'] * 57 + counts['}'] * 1197 + counts['>'] * 25137);
 */
--- a10-2.cs
+++ a10-2.cs
@@ -1,3 +1,66 @@
 ﻿/*
+Dictionary<char, char> braces = new()
+{
+    { '(', ')' },
+    { '[', ']' },
+    { '{', '}' },
+    { '<', '>' },
+};
 
+Dictionary<char, int> counts = new()
+{
+    { ')', 0 },
+    { ']', 0 },
+    { '}', 0 },
+    { '>', 0 },
+};
+
+Stack<char> stack = new();
+
+List<long> scores = new();
+
+while (true)
+{
+    string s = Console.ReadLine();
+    if (string.IsNullOrWhiteSpace(s)) break;
+
+    bool corrupt = false;
+    for (int i = 0; i < s.Length; i++)
+    {
+        if (braces.ContainsKey(s[i])) stack.Push(s[i]);
+        else
+        {
+            if (s[i] != braces[stack.Pop()])
+            {
+                counts[s[i]]++;
+                corrupt = true;
+                break;
+            }
+        }
+    }
+
+    if (!corrupt)
+    {
+        long score = 0;
+        while (stack.TryPop(out char c))
+        {
+            score *= 5;
+            score += c switch
+            {
+                '(' => 1,
+                '[' => 2,
+                '{' => 3,
+                '<' => 4,
+                _ => throw new NotImplementedException()
+            };
+        }
+
+        scores.Add(score);
+    }
+
+    stack.Clear();
+}
+
+var sorted = (from x in scores orderby x select x).ToArray();
+Console.WriteLine(sorted[sorted.Length / 2]);
 */
