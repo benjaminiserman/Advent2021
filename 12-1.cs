09cdb173e0a69d8a89c74ca7248eafa7192375e1
Benjamin E. Iserman <winggar1228@gmail.com>
2021-12-11 23:41:35 -0600

    day 12

--- 12-1.cs
+++ 12-1.cs
@@ -1,3 +1,38 @@
 ﻿/*
+List<(string, string)> connections = new();
 
+while (true)
+{
+    string s = Console.ReadLine();
+    if (string.IsNullOrWhiteSpace(s)) break;
+    string[] vs = s.Split('-');
+    connections.Add((vs[0], vs[1]));
+}
+
+Console.WriteLine(Traverse(connections, "start", "start"));
+
+int Traverse(List<(string, string)> connections, string path, string current)
+{
+    var found = from x in connections where x.Item1 == current || x.Item2 == current select x;
+    int count = 0;
+
+    foreach (var c in found)
+    {
+        string next = c.Item1 == current ? c.Item2 : c.Item1;
+
+        if (next == "start") continue;
+
+        if (next == "end")
+        {
+            //Console.WriteLine($"{path}-end");
+            count++;
+        }
+        else if (!path.Contains($"{current}-{next}"))
+        {
+            if (!char.IsLower(next[0]) || !path.Contains($"-{next}-")) count += Traverse(connections, $"{path}-{next}", next);
+        }
+    }
+
+    return count;
+}
 */
--- 12-2.cs
+++ 12-2.cs
@@ -1,3 +1,47 @@
 ﻿/*
+List<(string, string)> connections = new();
 
+while (true)
+{
+    string s = Console.ReadLine();
+    if (string.IsNullOrWhiteSpace(s)) break;
+    string[] vs = s.Split('-');
+    connections.Add((vs[0], vs[1]));
+}
+
+Console.WriteLine(Traverse(connections, "start", "start", null));
+
+int Traverse(List<(string, string)> connections, string path, string current, string small)
+{
+    var found = from x in connections where x.Item1 == current || x.Item2 == current select x;
+    int count = 0;
+
+    foreach (var c in found)
+    {
+        string next = c.Item1 == current ? c.Item2 : c.Item1;
+
+        //Console.WriteLine($"maybe {path}-{next}... {small ?? "null"}?");
+
+        if (next == "start") continue;
+
+        if (next == "end")
+        {
+            //Console.WriteLine($"{path}-end");
+            count++;
+        }
+        else if (!path.Contains($"{current}-{next}") || (small is null || current == small))
+        {
+            if (!char.IsLower(next[0]) || !path.Contains($"-{next}-") || small is null)
+            {
+                if (char.IsLower(next[0]) && path.Contains($"-{next}-"))
+                {
+                    count += Traverse(connections, $"{path}-{next}", next, next);
+                }
+                else count += Traverse(connections, $"{path}-{next}", next, small);
+            }
+        }
+    }
+
+    return count;
+}
 */
