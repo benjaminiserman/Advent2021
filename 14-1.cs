91d2b57b6df62fc599c1a3d622868747935f7adb
Benjamin E. Iserman <winggar1228@gmail.com>
2021-12-14 00:59:12 -0600

    day 14

--- 14-1.cs
+++ 14-1.cs
@@ -1,3 +1,40 @@
 ﻿/*
+string polymer = Console.ReadLine();
+Dictionary<string, char> pairs = new();
 
+Console.ReadLine();
+
+while (true)
+{
+    string s = Console.ReadLine();
+    if (string.IsNullOrWhiteSpace(s)) break;
+
+    string[] split = s.Split();
+
+    pairs.Add(split[0], split[2][0]);
+}
+
+string copy = polymer;
+
+for (int x = 0; x < 10; x++)
+{
+    for (int i = 1; i < copy.Length; i++)
+    {
+        if (pairs.TryGetValue($"{copy[i - 1]}{copy[i]}", out char c))
+        {
+            copy = copy.Insert(i, c.ToString());
+            i++;
+        }
+    }
+}
+
+Dictionary<char, int> counts = new();
+foreach (char c in copy)
+{
+    if (counts.ContainsKey(c)) counts[c]++;
+    else counts.Add(c, 1);
+}
+
+var sorted = (from x in counts.Values orderby x select x).ToArray();
+Console.WriteLine(sorted[^1] - sorted[0]);
 */
--- 14-2.cs
+++ 14-2.cs
@@ -1,3 +1,58 @@
 ﻿/*
+using System.Numerics;
 
+string polymer = Console.ReadLine();
+Dictionary<string, char> pairs = new();
+Dictionary<string, long> counts = new();
+Dictionary<char, long> letterCounts = new();
+
+Console.ReadLine();
+
+while (true)
+{
+    string s = Console.ReadLine();
+    if (string.IsNullOrWhiteSpace(s)) break;
+
+    string[] split = s.Split();
+
+    pairs.Add(split[0], split[2][0]);
+    counts.Add(split[0], 0);
+    if (!letterCounts.ContainsKey(split[0][0])) letterCounts.Add(split[0][0], 0);
+}
+
+string copy = polymer;
+
+for (int i = 1; i < copy.Length; i++)
+{
+    counts[$"{copy[i - 1]}{copy[i]}"]++;
+}
+
+for (int i = 0; i < 40; i++)
+{
+    Dictionary<string, long> dCounts = new();
+    foreach (var kvp in counts) dCounts.Add(kvp.Key, 0);
+
+    foreach (var kvp in counts)
+    {
+        string pair = kvp.Key;
+        char c = pairs[kvp.Key];
+
+        dCounts[$"{pair[0]}{c}"] += kvp.Value;
+        dCounts[$"{c}{pair[1]}"] += kvp.Value;
+    }
+
+    counts = dCounts;
+}
+
+foreach (var pair in counts)
+{
+    letterCounts[pair.Key[0]] += pair.Value;
+    letterCounts[pair.Key[1]] += pair.Value;
+}
+
+letterCounts[polymer[0]]++;
+letterCounts[polymer[^1]]++;
+
+var sorted = (from x in letterCounts.Values orderby x select x).ToArray();
+Console.WriteLine((sorted[^1] - sorted[0]) / 2);
 */
