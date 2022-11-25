d445656337359ed7c05e966a1d7a07d5700095d4
Benjamin E. Iserman <winggar1228@gmail.com>
2022-11-25 14:34:16 -0600

    refactor 3-2

---
+++ 03-2-EvenBetter.cs
@@ -0,0 +1,32 @@
+﻿using InputHandler; // wow I am am immensely better programmer than this time last year - me, 25NOV2022
+
+Console.WriteLine("Input:");
+var readings = Input.ListUntilWhiteSpace();
+var digits = readings.First().Length;
+
+int FilterReadings(IEnumerable<string> readings, Predicate<int> condition, string message)
+{
+    for (var i = 0; i < digits; i++)
+    {
+        var oneCount = readings.Count(x => x[i] == '1');
+        var useOne = oneCount - (readings.Count() - oneCount);
+
+        readings = readings.Where(line => (line[i] == '1') == condition(useOne)).ToList();
+        Console.WriteLine(readings.Count());
+
+        if (readings.Count() == 1)
+        {
+            var b = Convert.ToInt32(readings.First(), 2);
+
+            Console.WriteLine($"{message}: {b} with {readings.First()}");
+            return b;
+        }
+    }
+
+    return 0;
+}
+
+var oxygenMeter = FilterReadings(readings, useOne => useOne >= 0, "Oxygen");
+var co2Meter = FilterReadings(readings, useOne => useOne < 0, "CO2");
+
+Console.WriteLine($"Key: {oxygenMeter * co2Meter}");
